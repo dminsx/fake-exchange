3f5338d8f2fe260ba7db8d6fb29a5e1d1d3a6c28
dminsx <idminsx@gmail.com>
2026-07-08 18:11:22 +0300

    init app

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from "vue";
+const price = ref(500);
+
+setInterval(() => {
+  price.value +=
+    Math.random() * (price.value * 1.5 + price.value) - price.value;
+}, 2000);
+</script>
+
+<template>
+  <div>Price: ${{ price }}</div>
+</template>
+
+<style scoped></style>
